3b6be6e21dfbb199ef19721fdb6db6b465c2c569
rybinaanya <rybinaann@gmail.com>
2020-09-11 00:14:43 +0300

    My first calculator for the first homework

---
+++ Calculator.py
@@ -0,0 +1,34 @@
+####### Simple calculator performs one of the following operations with two entered numbers:
+# addition: +
+# subtraction: -
+# multiplication: *
+# division: /
+# modulus: %
+# exponentiation: ** or ^
+# floor division: //
+####### The code:
+# Enter 2 numbers and an operator
+Number1 = float(input('Enter the first number: '))
+Operator = input('Enter the operator: ')
+Number2 = float(input('Enter the second number: '))
+
+# Perform operation corresponding to the entered operator symbol
+if Operator == '*':
+    print(Number1 * Number2)
+elif Operator == '/':
+    print(Number1 / Number2)
+elif Operator == '+':
+    print(Number1 + Number2)
+elif Operator == '-':
+    print(Number1 - Number2)
+elif Operator == '%':
+    print(Number1 % Number2)
+elif Operator == '//':
+    print(Number1 // Number2)
+elif Operator == '**' or Operator == '^':
+    print(Number1 ** Number2)
+else:
+    print(f'Error: operator "{Operator}" not found.',
+          f'Only the following operators can be used in this calculator:\n',
+          f'"+" (addition), "-" (subtraction), "*" (multiplication), "/" (division),\n',
+          f'"%" (modulus),"**" or "^" (exponentiation),"//" (floor division).')
